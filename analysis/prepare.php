d9fa07c7f02958750d631720c67d76d6b417a35a
Alexey Baturin <alexey.baturin.nsk@gmail.com>
2011-05-08 00:01:58 +0700

    arguments to prepare tool, no dependency on config

--- analysis/prepare.php
+++ analysis/prepare.php
@@ -1,9 +1,10 @@
 <?php
 
-require_once 'config.php';
 require_once 'common.php';
 require_once 'classes/ReduceFileToFile.php';
 require_once 'classes/ExtensionFileFilter.php';
+require_once 'classes/FilesDirectoryIterator.php';
+require_once 'classes/AllFileFilter.php';
 
 class SummarizeCoverageDataProcessor {
     private $coverageFiles;
@@ -15,7 +16,6 @@ class SummarizeCoverageDataProcessor {
 
     public function process($fileName)
     {
-        global $config;
         $fileCoverage = array();
         $fileExecuted = false;
 
@@ -85,9 +85,31 @@ class SummarizeCoverageDataProcessor {
     }
 }
 
-$coverageFiles = glob($config['COVERAGE_FILES_DIR'] . DIRECTORY_SEPARATOR . '*');
+function printUsage() {
+    echo 'Utility that prepares raw XDebug coverage data for report-*.php tools.' . PHP_EOL;
+    echo 'Usage: ' . PHP_EOL;
+    echo '  php prepare.php SOURCES_DIRECTORY RAW_DATA_DIRECTORY PREPROCESSED_DATA_DIRECTORY' . PHP_EOL;
+    echo '    where:' . PHP_EOL;
+    echo '      SOURCES_DIRECTORY - directory with tested product sources (in the same location as on a computer with XDebug)' . PHP_EOL;
+    echo '      RAW_DATA_DIRECTORY - directory with raw php coverage data (produced by XDebug and serialized by prepend.php script)' . PHP_EOL;
+    echo '      PREPROCESSED_DATA_DIRECTORY - directory to put pre-processed data that could be used by report-*.php tools' . PHP_EOL;
+    echo '    all directories must not contain trailing slash' . PHP_EOL;
+}
+
+if (count($argv) != 4) {
+    echo 'Invalid arguments.' . PHP_EOL;
+    echo PHP_EOL;
+    printUsage();
+    exit(1);
+}
+
+$sourcesDir = $argv[1];
+$rawDataDir = $argv[2];
+$preprocessedDataDir = $argv[3];
+
+$coverageFiles = new FilesDirectoryIterator($rawDataDir, new AllFileFilter());
 $fileProcessor = new SummarizeCoverageDataProcessor($coverageFiles);
 $reduceFileToFile = new ReduceFileToFile();
-$reduceFileToFile->process($config['SOURCES_DIR'], $config['RESULTS_DIR'], '.line-coverage', $fileProcessor, new ExtensionFileFilter('php'));
+$reduceFileToFile->process($sourcesDir, $preprocessedDataDir, '.line-coverage', $fileProcessor, new ExtensionFileFilter('php'));
 
 ?>
