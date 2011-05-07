1e08fa46d4e3ba3b0b4a39c4b0e43125185e9eba
Alexey Baturin <alexey.baturin.nsk@gmail.com>
2011-05-08 00:17:44 +0700

    removed config.php that is not necessary anymore

--- analysis/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$config['COVERAGE_FILES_DIR'] = '/tmp/php-code-coverage';
-$config['SOURCES_DIR'] = '/home/alexey/phppgadmin/phppgadmin';
-$config['RESULTS_DIR'] = '/tmp/results';
-$config['LINES_ANALYZED_FILE'] = '/tmp/lines-analyzed';
-
-?>
